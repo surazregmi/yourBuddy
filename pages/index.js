705a7fe66ebed8d801e3e9daf8cedebe7cd031f3
suraj regmi <hyakoste@gmail.com>
2024-09-09 22:49:16 +1000

    add home page design with login and signup.

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,62 @@
 import Head from "next/head";
 import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import { getSession } from "@auth0/nextjs-auth0";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRobot, faComment } from "@fortawesome/free-solid-svg-icons";
+
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Next JS ChatGPT Starter</title>
-      </Head>
-      <h1>Welcome to the Next JS &amp; ChatGPT Starter</h1>
+  const { isLoading, error, user } = useUser();
 
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
+  return (
+    <>
+    <Head>
+      <title>YourBuddy - Login or Signup</title>
+    </Head>
+    <div className="flex min-h-screen w-full items-center justify-center bg-gray-200 text-center text-black">
       <div>
-        <Link href="api/auth/login">Login</Link>
+        <div>
+          <FontAwesomeIcon
+            icon={faComment}
+            className="mb-2 text-4xl text-blue-300"
+          />
+          
+        </div>
+        <h1 className="text-4xl font-bold">Welcome to YourBuddy</h1>
+        <p className="mt-2 text-lg">Log in with your account to continue chatting</p>
+        <div className="mt-4 flex justify-center gap-3">
+          {!user && (
+            <>
+              <Link href="/api/auth/login" className="btn">
+                Login
+              </Link>
+              <Link href="/api/auth/signup" className="btn">
+                Signup
+              </Link>
+            </>
+          )}
+        </div>
       </div>
     </div>
+  </>
   );
 }
+
+export const getServerSideProps = async (ctx) => {
+  const session = await getSession(ctx.req, ctx.res);
+  if (!!session) {
+    return {
+      redirect: {
+        destination: "/chat",
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
